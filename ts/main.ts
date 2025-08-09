71c4ae9d236452178b25b3e473c04f7bb7560c97
anvaperi <donantonio5314@gmail.com>
2025-08-09 09:25:22 +0200

    class SalesEmployee created

--- ts/main.ts
+++ ts/main.ts
@@ -4,3 +4,76 @@ interface Employee {
   email: string;
   birthDate: Date;
 }
+
+class SalesEmployee implements Employee {
+  name: string;
+  surname: string;
+  email: string;
+  birthDate: Date;
+  sdUnit: string;
+  area: string;
+
+  constructor(
+    name: string,
+    surname: string,
+    email: string,
+    birthDate: Date,
+    sdUnit: string,
+    area: string
+  ) {
+    this.name = name;
+    this.surname = surname;
+    this.email = email;
+    this.birthDate = birthDate;
+    this.sdUnit = sdUnit;
+    this.area = area;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  setName(newName: string) {
+    this.name = newName;
+  }
+
+  getSurname(): string { 
+    return this.surname;
+  }
+
+  setSurname(newSurname: string) {
+    this.surname = newSurname
+  }
+
+  getEmail(): string { 
+    return this.email;
+  }
+
+  setEmail(newEmail: string) {
+    this.email = newEmail;
+  }
+
+  getBirthDate(): Date { 
+    return this.birthDate;
+  }
+
+  setBirthDate(newBirthdate: Date) {
+    this.birthDate = newBirthdate;
+  }
+
+  getSdUnit(): string { 
+    return this.sdUnit;
+  }
+
+  setSdUnit(newSdUnit: string) {
+    this.sdUnit = newSdUnit;
+  }
+
+  getArea(): string { 
+    return this.area;
+  }
+
+  setArea(newArea: string) {
+    this.area = newArea;
+  }
+}
